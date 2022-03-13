50806f7255365405d70b340887dc17b9dd26af65
Taewoo-cho <tojesus623@gmail.com>
2022-03-14 00:16:46 +0900

    상세페이지, 글쓰기 페이지 디자인 초안

--- Wub/src/main/java/com/bitc/wub/controller/WubArticleController.java
+++ Wub/src/main/java/com/bitc/wub/controller/WubArticleController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.bitc.wub.dto.ArticleDto;
@@ -29,14 +30,15 @@ public class WubArticleController {
 	}
 	
 	// 글쓰기 페이지에서 DB로 INSERT(이미지 첨부)
-	/*
-	 * @RequestMapping(value="/article/write", method=RequestMethod.POST) public
-	 * String insertArticle(ArticleDto articleDto, MultipartHttpServletRequest
-	 * multiFile) {
-	 * 
-	 * //articleService.insertArticle(articleDto, multiFile); return
-	 * "redirect:/mypage"; }
-	 */
+	
+	@RequestMapping(value="/article/write", method=RequestMethod.POST) 
+	public String insertArticle(ArticleDto articleDto, MultipartHttpServletRequest multiFile) {
+	
+		//articleService.insertArticle(articleDto, multiFile); 
+		//int idx = articleService.selectIdx() -> 멤버 dto의 아이디를 받아와서 가장 최신글로 리다이렉트
+		return "redirect:/mypage"; 
+	}
+	
 
 	// 상세 글 읽기
 	@RequestMapping(value = "/article/{articleIdx}", method=RequestMethod.GET)
