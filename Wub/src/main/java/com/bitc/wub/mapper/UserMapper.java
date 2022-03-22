727d36ebb21c13cf66b7dab3d7377de1e31dac06
atoka29 <91643582+atoka29@users.noreply.github.com>
2022-03-22 21:26:03 +0900

    마이페이지 (회원 탈퇴 제외)

--- Wub/src/main/java/com/bitc/wub/mapper/UserMapper.java
+++ Wub/src/main/java/com/bitc/wub/mapper/UserMapper.java
@@ -5,13 +5,18 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.bitc.wub.dto.UserArticleDto;
+import com.bitc.wub.dto.UserCommentDto;
 import com.bitc.wub.dto.UserDto;
+import com.bitc.wub.dto.UserRecommendDto;
 
 @Mapper
 public interface UserMapper {
 	
 //	로그인
-	int login(UserDto user) throws Exception;
+	public int login(UserDto user) throws Exception;
+	
+//	정보 가져오기
+	public UserDto userInfo(String userId) throws Exception;
 
 //	회원 가입
 	public void insertUser(UserDto user) throws Exception;
@@ -22,11 +27,20 @@ public interface UserMapper {
 //	회원 탈퇴
 	public void deleteUser(int userIdx) throws Exception;
 	
+//	회원 정보 수정 페이지
+	public UserDto openUser(int userIdx) throws Exception;
+	
 //	회원 정보 수정
 	public void updateUser(UserDto user) throws Exception;
-
+	
 //	내가 작성한 판매글
-	public List<UserArticleDto> selectUserArticle() throws Exception;
+	public List<UserArticleDto> selectUserArticle(int userIdx) throws Exception;
+
+//	마음에 드는 판매글
+	public List<UserRecommendDto> selectUserRecommend(int userIdx) throws Exception;
+
+//	내가 작성한 댓글
+	public List<UserCommentDto> selectUserComment(int userIdx) throws Exception;
 	
 	
 
